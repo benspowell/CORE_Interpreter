ea7e609a3147d72e1a261e12d1d1a8ede3392349
Ben Powell <powell.907@osu.edu>
2019-11-09 13:04:38 -0500

    add assignnment documents

--- src/com/benspowell/core_interpreter/tokenizer/TokenizerTest.java
+++ src/com/benspowell/core_interpreter/tokenizer/TokenizerTest.java
@@ -1,6 +1,6 @@
-package edu.c3341;
+package com.benspowell.core_interpreter.tokenizer;
 
-import static edu.c3341.TokenKind.ERROR;
+import com.benspowell.core_interpreter.tokenizer.*;
 
 import java.io.IOException;
 import java.nio.file.Paths;
@@ -9,7 +9,7 @@ import java.util.Scanner;
 /**
  * Part 1 of Project 1 for CSE 3341. Test a Tokenizer for Core.
  *
- * @author Wayne D. Heym
+ * @author Benjamin S. Powell
  *
  */
 public final class TokenizerTest {
@@ -27,7 +27,7 @@ public final class TokenizerTest {
      *            the command line arguments
      */
     public static void main(String[] args) {
-        Scanner in;
+    	Scanner in;
         try {
             in = new Scanner(Paths.get(args[0]));
         } catch (IOException e) {
@@ -35,11 +35,14 @@ public final class TokenizerTest {
             return;
         }
         Tokenizer t = new Tokenizer(in);
-        while (t.getToken() != TokenKind.EOF && t.getToken() != ERROR) {
+        while (t.getToken() != TokenKind.EOF && t.getToken() != TokenKind.ERROR) {
             System.out.println(t.getToken().testDriverTokenNumber());
             t.skipToken();
         }
-        if (t.getToken() == ERROR) {
+        if (t.getToken() == TokenKind.EOF) {
+        	System.out.println(t.getToken().testDriverTokenNumber());
+        }
+        if (t.getToken() == TokenKind.ERROR) {
             System.out.println("Error: Illegal token encountered.");
         }
         /*
